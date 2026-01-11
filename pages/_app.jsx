e632dc9dffbcc65fa6b853b1e326db2b7f342772
Aditya Chitoshiya <adityachitoshiya@Adis-MacBook.local>
2026-01-11 11:33:58 +0530

    Fix: Mobile UX Optimization (Disable Zoom, Remove Touch Delay)

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -68,7 +68,7 @@ function MyApp({ Component, pageProps }) {
     return (
       <>
         <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, viewport-fit=cover" />
           <link rel="icon" href="/favicon-circle.png" />
         </Head>
         <Component {...pageProps} />
